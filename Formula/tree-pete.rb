2d8980a6765c69016f1048df38b8608122547240
Yofriadi Yahya <yahya.yofriadi@hendrickscorp.com>
2026-01-29 13:45:35 +0700

    Add tree-pete formula (v1.3.0)
    Add peteretelej/tree CLI - a cross-platform tree command
    implemented in Rust. Supports both Intel (amd64) and Apple
    Silicon (arm64) macOS architectures.
    
    Features:
    - Display directory structure in tree format
    - Control recursion depth with -L flag
    - Show hidden files with -a flag
    - Colorize output with -C flag
    - Display file sizes with -s and -h flags

---
+++ Formula/tree-pete.rb
@@ -0,0 +1,22 @@
+class TreePete < Formula
+  desc "Cross-platform tree CLI implemented in Rust by peteretelej"
+  homepage "https://github.com/peteretelej/tree"
+  version "1.3.0"
+  license "MIT"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/peteretelej/tree/releases/download/v#{version}/tree-v#{version}-macOS-arm64.tar.gz"
+    sha256 "dd11cd1bf0fe6ae06e4a624a3b4084c0fe503ae473c9fb3ab1ac53ed4e25a5c3"
+  else
+    url "https://github.com/peteretelej/tree/releases/download/v#{version}/tree-v#{version}-macOS-amd64.tar.gz"
+    sha256 "80637807d628a3d57fabc2fb56db3c341c26ced3fdc5f429429f21424be29f3b"
+  end
+
+  def install
+    bin.install "tree" => "tree-pete"
+  end
+
+  test do
+    assert_match "tree", shell_output("#{bin}/tree-pete --version")
+  end
+end
